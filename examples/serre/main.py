4cfd2194f9ef36ce55dc3a825be56ab854d2e468
FredThx <fredthx@gmail.com>
2023-03-05 17:00:07 +0100

    divers test

--- examples/serre/main.py
+++ examples/serre/main.py
@@ -1,31 +1,33 @@
 # main.py -- put your code here!
 
 
-from ds18b20 import DS18b20
-from fdht import DHT22
-from fluxldr import LuxLDR
+from devices.ds18b20 import DS18b20
+from devices.fdht import DHT22
+from devices.fluxldr import LuxLDR
+from machine import Pin
 
-
-from fmpyiot.fmpyiot import *
+from fmpyiot.fmpyiot import FmPyIot
 from fmpyiot.topics import Topic
 
 ds = DS18b20(14)
 dht = DHT22(16)
 ldr = LuxLDR(channel = 0, R= 10_000, k=0.9)
+led = Pin(17,Pin.OUT)
 
 iot = FmPyIot(            
-    mqtt_host = "192.168.10.150",
+    mqtt_host = "***REMOVED***",
     mqtt_base_topic = "T-HOME/SERRE",
     ssid = 'WIFI_THOME2',
     password = "***REMOVED***",
-    watchdog=None,
+    watchdog=100,
     sysinfo_period = 600,
     async_mode=False)
 
-#iot.wd.disable()
-temperature = Topic("./temperatures", read=ds.read_all, send_period=20)
-humidity = Topic("./humidity", read = dht.read_humidity, send_period=20)
-luminosite = Topic("./luminosite", read = ldr.read, send_period=20)
+temperature = Topic("./temperatures", read=lambda topic, payload : ds.read_all(), send_period=60)
+humidity = Topic("./humidity", read = lambda topic, payload : dht.read_humidity(), send_period=60)
+luminosite = Topic("./luminosite", read = lambda topic, payload : ldr.read(), send_period=60)
+led_topic = Topic('./LED', action = lambda payload : led.value(int(payload)))
+
 
 if True:
     while not iot.connect():
@@ -33,4 +35,5 @@ if True:
     iot.add_topic(temperature)
     iot.add_topic(humidity)
     iot.add_topic(luminosite)
+    iot.add_topic(led_topic)
     iot.run()
