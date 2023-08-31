1feafa3c6bc2d8852921b2b1993d22af7710e915
FredericOlfa <frederic.thome@olfa.fr>
2023-08-31 17:23:29 +0200

    enrouleur : oups

--- examples/enrouleur/main.py
+++ examples/enrouleur/main.py
@@ -1,1 +1,25 @@
-# main.py -- put your code here!
+from machine import Pin
+from devices.mp6550 import MP6550
+from enrouleur import Enrouleur
+
+moteur = MP6550(
+    pinIN1=17,
+    pinIN2=16,
+    pinVISEN=26)
+
+moteur.set_speed(0.9)
+
+detecteur = Pin(15, Pin.ALT)
+pin_forward = Pin(13, Pin.IN, Pin.PULL_UP)
+pin_backward = Pin(14, Pin.IN, Pin.PULL_UP)
+pin_force_forward = Pin(11, Pin.IN, Pin.PULL_UP)
+pin_force_backward = Pin(12, Pin.IN, Pin.PULL_UP)
+                   
+enrouleur = Enrouleur(
+    moteur = moteur,
+    detecteur=detecteur, temporisation = 1,
+    pin_forward=pin_forward,
+    pin_backward=pin_backward,
+    pin_force_forward=pin_force_forward,
+    pin_force_backward=pin_force_backward)
+enrouleur.run()
