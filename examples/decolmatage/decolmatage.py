c2ba902018c357bb840f5ea68c95fc18bdfc2bee
FredThx <fredthx@gmail.com>
2023-09-29 07:10:23 +0200

    fix : decolmatage version timers

--- examples/decolmatage/decolmatage.py
+++ examples/decolmatage/decolmatage.py
@@ -9,27 +9,35 @@ class Decolmatage:
             self, 
             relais:list[Pin], tempo1:float=30, tempo2:float=0.4):
         self.relais = relais
-        self.tempo1 = int(tempo1) #second
-        self.tempo2 = int(tempo2 * 1000) #ms
         for i, rel in enumerate(self.relais):
             if type(rel)==int:
                 self.relais[i]=Pin(rel, Pin.OUT)
             else:
                 rel.init(Pin.OUT)
+        self.tempo1 = int(tempo1*1000) #ms
+        self.tempo2 = int(tempo2 * 1000) #ms
+        self.tim1 = Timer()
+        self.tim2 = Timer()
+
     
     def souffle(self, rel:Pin):
         print(f"Souffle on {rel}")
         rel.on()
-        tim = Timer()
-        tim.init(period = self.tempo2,
+        self.tim2.init(period = self.tempo2,
                  mode=Timer.ONE_SHOT,
                  callback=lambda x:rel.off())
+    
+    def next_souffle(self, tim:Timer):
+        rel = self.relais.pop(0)
+        self.souffle(rel)
+        self.relais.append(rel)
 
     def run(self):
-        while True:
-            for rel in self.relais:
-                self.souffle(rel)
-                time.sleep(self.tempo1)
+        self.tim1.init(period = self.tempo1,
+                       mode = Timer.PERIODIC,
+                       callback = self.next_souffle)
+    def stop(self):
+        self.tim1.deinit()
 
 if __name__=='__main__':
     decolmate = Decolmatage([25],10,1)
--- examples/decolmatage/main.py
+++ examples/decolmatage/main.py
@@ -2,5 +2,6 @@ from machine import Pin
 from decolmatage import Decolmatage
 
 
-decolmate = Decolmatage([Pin(18),Pin(19),Pin(20),Pin(21)],10,0.4)
+decolmate = Decolmatage([Pin(18),Pin(19),Pin(20),Pin(21)],60,0.4)
+#decolmate = Decolmatage([Pin(25),Pin(19),Pin(25),Pin(21)],10,0.4)
 decolmate.run()
