f1ad4e0bde8d22598ca8c3c9c7b4dc8f2c3f6d5c
FredericOlfa <frederic.thome@olfa.fr>
2023-09-29 10:09:17 +0200

    feat : decolmatage : add led

--- examples/decolmatage/decolmatage.py
+++ examples/decolmatage/decolmatage.py
@@ -7,7 +7,7 @@ class Decolmatage:
     '''
     def __init__(
             self, 
-            relais:list[Pin], tempo1:float=30, tempo2:float=0.4):
+            relais:list[Pin], tempo1:float=30, tempo2:float=0.4, led = None):
         self.relais = relais
         for i, rel in enumerate(self.relais):
             if type(rel)==int:
@@ -18,6 +18,24 @@ class Decolmatage:
         self.tempo2 = int(tempo2 * 1000) #ms
         self.tim1 = Timer()
         self.tim2 = Timer()
+        self.led = led
+        self.led.init(Pin.OUT)
+        self.tim_led = Timer()
+        if self.led:
+            tim_led = Timer()
+            tim_led.init(
+                period = 1000,
+                mode = Timer.PERIODIC,
+                callback = self.blink_led
+            )
+
+    def blink_led(self, tim:Timer):
+        self.led.on()
+        self.tim_led.init(
+            period = 200,
+            mode = Timer.ONE_SHOT,
+            callback = lambda t:self.led.off()
+        )
 
     
     def souffle(self, rel:Pin):
--- examples/decolmatage/main.py
+++ examples/decolmatage/main.py
@@ -2,6 +2,10 @@ from machine import Pin
 from decolmatage import Decolmatage
 
 
-decolmate = Decolmatage([Pin(18),Pin(19),Pin(20),Pin(21)],60,0.4)
+decolmate = Decolmatage(
+        relais=[Pin(18),Pin(19),Pin(20),Pin(21)],
+        tempo1=60,
+        tempo2=0.4,
+        led = Pin(25))
 #decolmate = Decolmatage([Pin(25),Pin(19),Pin(25),Pin(21)],10,0.4)
 decolmate.run()
