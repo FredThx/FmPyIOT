0ede75a8d2fdd10feebbd05029a69bb743730659
FredThx <fredthx@gmail.com>
2023-10-12 23:58:26 +0200

    feat : visseuse

---
+++ examples/visseuse/main.py
@@ -0,0 +1,31 @@
+from machine import Pin
+
+
+
+proxi = Pin(17, Pin.IN)
+led = Pin(16,Pin.OUT)
+relais = Pin(14,Pin.OUT)
+buzzer = Pin(15, Pin.OUT)
+bt_reset = Pin(13,Pin.IN)
+
+
+def on_pin_change(pin):
+    print(pin)
+    if pin == bt_reset:
+        led.off()
+        relais.off()
+        buzzer.off()
+    else:
+        led.on()
+        relais.on()
+        buzzer.on()
+
+proxi.irq(
+    on_pin_change,
+    Pin.IRQ_FALLING
+    )
+
+bt_reset.irq(
+    on_pin_change,
+    Pin.IRQ_FALLING
+    )
