fc8b535043a90906147565612bffacb2da23863f
FredThx <fredthx@gmail.com>
2023-10-29 17:41:20 +0100

    rangement des test

--- examples/test_async_web/main.py
+++ examples/test_async_web/main.py
@@ -1,97 +1,60 @@
-
-import time
-import uasyncio as asyncio
-from machine import Pin
-from fmpyiot.fmpyiot import FmPyIot
-from fmpyiot.topics import Topic, TopicRoutine, TopicIrq, TopicOnChange
-
-
-time.sleep(5)
+#!/usr/bin/env micropython
+"""
+MIT license
+(C) Konstantin Belyalov 2017-2018
+"""
+import tinyweb
 
 
-iot = FmPyIot(
-    mqtt_host = "***REMOVED***",
-    mqtt_base_topic = "T-HOME/TEST",
-    ssid = 'WIFI_THOME2',
-    password = "***REMOVED***",
-    watchdog=None,
-    sysinfo_period = 600,
-    #led_wifi='LED',
-    )
-
-led = Pin('LED')
-led_blink = False
-
-async def blink_led(_,val):
-    print(f"Blink = {val}")
-    global led_blink
-    global led
-    led_blink = int(val)==1
-    while led_blink:
-        led.on()
-        await asyncio.sleep_ms(300)
-        led.off()
-        await asyncio.sleep(1)
-
-async def long_calculus(topic, payload):
-    print("start long calculus")
-    await asyncio.sleep(10)
-    print("long calculus done")
-    return int(payload)+1
-
-async def short_calculus(topic, payload):
-    print("start short calculus")
-    await asyncio.sleep_ms(50)
-    print("short calculus done")
-    return int(payload)**2
-
-async def calc(topic, payload=None):
-    payload = payload or 42
-    print(f"calc({topic},{payload})")
-    l=await asyncio.gather(long_calculus(topic, payload), short_calculus(topic, payload))
-    print(f"Result des calculs : {l}")
-    return sum(l)
-
-# Un topic mode synchrone (pas la peine pour allumer une led)
-topic_led = Topic("./LED", action = lambda topic, payload: led(int(payload)))
-iot.add_topic(topic_led)
-
-# Topics en asynchone
-topic_blink_led = Topic("./LED_BLINK", action = blink_led)
-iot.add_topic(topic_blink_led)
-
-# A la fois vrai Topic (qui est activé par reception MQTT) et routine (qui est activé toutes les x secondes)
-topic_calc = Topic("./calc", read = calc, send_period=5)
-iot.add_topic(topic_calc)
+# Create web server application
+app = tinyweb.webserver()
 
 
-# Des routines sans aucun topic
-async def routine_blink():
-    while True:
-        await asyncio.sleep(10)
-        print("Routine : blink on!")
-        await iot.publish_async("./LED_BLINK",1)
-        await asyncio.sleep(5)
-        print("Routine : blink off!")
-        await iot.publish_async("./LED_BLINK",0)
-topic_routine_blink = TopicRoutine(routine_blink)
-iot.add_routine(topic_routine_blink)
+# Index page
+@app.route('/')
+async def index(request, response):
+    # Start HTTP response with content-type text/html
+    await response.start_html()
+    # Send actual HTML page
+    await response.send('<html><body><h1>Hello, world! (<a href="/table">table</a>)</h1></html>\n')
 
-#Une detection de changement d'état sur une Pin
-iot.add_topic(TopicIrq("./LED", pin=Pin(15,Pin.IN), trigger = Pin.IRQ_RISING, values=("1","0")))
 
-#Un TopicOnChange
-g_compteur = 0
-def get_compteur():
-    global g_compteur
-    g_compteur += 1
-    return g_compteur
+# HTTP redirection
+@app.route('/redirect')
+async def redirect(request, response):
+    # Start HTTP response with content-type text/html
+    await response.redirect('/')
 
-def raz_compteur():
-    global g_compteur
-    g_compteur = 0
 
-iot.add_topic(TopicOnChange("./COMPTEUR",read = get_compteur, variation=10, period=1))
-iot.add_topic(Topic("./COMPTEUR", action = raz_compteur))
+# Another one, more complicated page
+@app.route('/table')
+async def table(request, response):
+    # Start HTTP response with content-type text/html
+    await response.start_html()
+    await response.send('<html><body><h1>Simple table</h1>'
+                        '<table border=1 width=400>'
+                        '<tr><td>Name</td><td>Some Value</td></tr>')
+    for i in range(10):
+        await response.send('<tr><td>Name{}</td><td>Value{}</td></tr>'.format(i, i))
+    await response.send('</table>'
+                        '</html>')
 
-iot.run()
+
+def run():
+    app.run(host='0.0.0.0', port=8081)
+
+
+if __name__ == '__main__':
+    run()
+    # To test your server:
+    # - Terminal:
+    #   $ curl http://localhost:8081
+    #   or
+    #   $ curl http://localhost:8081/table
+    #
+    # - Browser:
+    #   http://localhost:8081
+    #   http://localhost:8081/table
+    #
+    # - To test HTTP redirection:
+    #   curl http://localhost:8081/redirect -v
