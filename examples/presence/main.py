9788441b5a236082c9d2d5497987cc7877d55364
FredThx <fredthx@gmail.com>
2024-01-06 16:56:54 +0100

    feat : detection => FmPyIotWeb

--- examples/presence/main.py
+++ examples/presence/main.py
@@ -1,13 +1,14 @@
 from machine import Pin
-import time
-from fmpyiot.fmpyiot import FmPyIot
+import time, logging
+from fmpyiot.fmpyiot_web import FmPyIotWeb
 from fmpyiot.topics import Topic, TopicIrq
 
 detecteur = Pin(15,Pin.IN)
 
 time.sleep(5)
 
-iot = FmPyIot(            
+
+iot = FmPyIotWeb(
     mqtt_host = "***REMOVED***",
     mqtt_base_topic = "T-HOME/BUREAU/DETECTION",
     ssid = 'WIFI_THOME2',
@@ -15,8 +16,13 @@ iot = FmPyIot(
     watchdog=100,
     sysinfo_period = 600,
     led_incoming="LED", #internal
-    led_wifi=16
+    led_wifi=16,
+    web=True,
+    web_credentials=(***REMOVED***, ***REMOVED***),
+    name = "Bureau Detection",
+    logging_level=logging.INFO,
     )
+
 
 detection_topic = TopicIrq("./detect", pin=detecteur, trigger = Pin.IRQ_RISING)
 
