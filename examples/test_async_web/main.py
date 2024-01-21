f98ba97913e06cfd6364ad4a8b1278b43d6024d1
FredThx <fredthx@gmail.com>
2024-01-21 20:26:45 +0100

    feat : iot test_async : add display

--- examples/test_async_web/main.py
+++ examples/test_async_web/main.py
@@ -1,13 +1,19 @@
 
-import time
+import time, json
 import uasyncio as asyncio
-from machine import Pin, ADC
+from machine import Pin, ADC, SPI
 from fmpyiot.fmpyiot_web import FmPyIotWeb
 from fmpyiot.topics import Topic, TopicAction, TopicRoutine, TopicIrq, TopicOnChange, TopicRead
 import logging
+from lcd12864 import SPI_LCD12864
 
 time.sleep(5)
+try:
+    del len #Bug micropython!!!
+except:
+    pass
 
+assert len([])==0, "Error with len!"
 
 iot = FmPyIotWeb(
     mqtt_host = "***REMOVED***",
@@ -20,7 +26,7 @@ iot = FmPyIotWeb(
     web=True,
     web_credentials=(***REMOVED***, ***REMOVED***),
     name = "FmPyIot TEST",
-    logging_level=logging.INFO,
+    logging_level=logging.DEBUG,
     )
 
 led = Pin('LED')
@@ -67,5 +73,66 @@ async def run_leds_once():
         leds[i].value(0)
 
 iot.add_topic(TopicRoutine(run_leds_once, send_period=5))
+
+#Un ecran
+lcd = SPI_LCD12864(
+        spi=SPI(0, polarity=0, phase = 1,bits=8, sck=Pin(6), mosi=Pin(7)),
+        cs=Pin(5, Pin.OUT, value = 0)
+    )
+
+lcd.rect(0,0,128,64,1)
+lcd.rect(3,3,128-6,64-6,1)
+lcd.update()
+
+HEART_ICONS = [
+    [
+        [0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,1,1,1,0,1,1,1,0,0],
+        [0,1,1,0,1,1,1,1,1,1,0],
+        [0,1,0,1,1,1,1,1,1,1,0],
+        [0,1,1,1,1,1,1,1,1,1,0],
+        [0,0,1,1,1,1,1,1,1,0,0],
+        [0,0,0,1,1,1,1,1,0,0,0],
+        [0,0,0,0,1,1,1,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0] ],
+    [
+        [0,0,1,1,0,0,0,1,1,0,0],
+        [0,1,1,1,1,0,1,1,1,1,0],
+        [1,1,1,0,1,1,1,1,1,1,1],
+        [1,1,0,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1],
+        [0,1,1,1,1,1,1,1,1,1,0],
+        [0,0,1,1,1,1,1,1,1,0,0],
+        [0,0,0,1,1,1,1,1,0,0,0],
+        [0,0,0,0,1,1,1,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0,0] ],
+  ]
+
+def draw_icon( lcd, from_x, from_y, icon ):
+    for y, row in enumerate( icon ):
+        for x, color in enumerate( row ):
+            if color==None:
+                continue
+            lcd.pixel( from_x+x,
+                       from_y+y,
+                       color )
+
+async def heart_async():
+    while True:
+        for i in range(len(HEART_ICONS)):
+            draw_icon(lcd, 1,1,HEART_ICONS[i])
+            lcd.update()
+            await asyncio.sleep(0.5)
+
+iot.add_topic(TopicRoutine(heart_async))
+
+def draw_text(lcd, payload):
+    payload = json.loads(payload)
+    print(f"payload = {payload} type = {type(payload)}")
+    lcd.text(payload['text'], payload['x'],payload['y'],1)
+    lcd.update()
+
+iot.add_topic(TopicAction("./text", lambda _topic, payload : draw_text(lcd, payload)))
 
 iot.run()
