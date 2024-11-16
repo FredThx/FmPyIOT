c4670ca8192664a8c4beaa72b07fb6e048f0b37b
fredthx <fredthx@gmail.com>
2024-11-16 20:46:20 +0100

    feat : salon detecteur

--- examples/salon/main.py
+++ examples/salon/main.py
@@ -1,30 +1,26 @@
 from machine import Pin, I2C
 import time, logging
 from fmpyiot.fmpyiot_web import FmPyIotWeb
-from fmpyiot.topics import Topic
+from fmpyiot.topics import Topic, TopicIrq
 from devices.bmp280 import BMP280
 from devices.ds18b20 import DS18b20
 
 from credentials import CREDENTIALS
 
 class Salon:
-    params = {
-        'param1' : 1,
-        "param2" : 2
-    }
+    
     def __init__(self):
         self.i2c = I2C(0, scl=Pin(5), sda=Pin(4), freq=400_000)
         self.bmp = BMP280(self.i2c)
         self.ds = DS18b20(27)
+        self.detecteur = Pin(26)
+        self.params = {}
 
     def get_pressure(self, **kwargs):
         return self.bmp.pressure/100
     def get_temperature(self, **kwargs):
         return self.ds.read_async()
 
-    def get_param(self, param:str):
-        return self.params[param]
-    
     def load_params(self, param:dict):
         logging.info("SALON : LOAD PARAMS")
         self.params.update(param)
@@ -50,17 +46,13 @@ iot = FmPyIotWeb(
 
 iot.set_param('salon', default=salon.params, on_change=salon.load_params)
 
-#detection_topic = TopicIrq("./detect", pin=detecteur, trigger = Pin.IRQ_RISING)
-#iot.add_topic(detection_topic)
+detection_topic = TopicIrq("./detect", pin=salon.detecteur, trigger = Pin.IRQ_RISING + Pin.IRQ_FALLING)
 topic_pression = Topic("./PRESSION", read=salon.get_pressure, send_period=30)
 topic_temperature = Topic("./temperature", read=salon.get_temperature, send_period=30)
 
 iot.add_topic(topic_pression)
 iot.add_topic(topic_temperature)
-
-iot.add_topic(Topic("./param1", read = lambda *args, **kwargs:salon.get_param("param1"), send_period=5))
-iot.add_topic(Topic("./param2", read = lambda *args, **kwargs:salon.get_param("param2"), send_period=5))
-
+iot.add_topic(detection_topic)
 
 iot.run()
 
