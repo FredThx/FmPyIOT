0013bf6340d746eddf47b838062208123d750d1c
fredthx <fredthx@gmail.com>
2024-12-01 17:22:59 +0100

    feat : bargraph + divers display

--- devices/display.py
+++ devices/display.py
@@ -24,7 +24,7 @@ class Display:
         except:
             self.device.show()
 
-    def set_widget(self, topic:str, widget:Field|Icon, payload:str = None):
+    def set_widget(self, topic:str, widget:Widget, payload:str = None):
         widget.root = self
         if topic not in self.topics:
             self.topics[topic] = []
@@ -48,7 +48,7 @@ class Display:
         '''
         for i in range(len(icon[0])):
             for j in range(len(icon)):
-                self.device.pixel(x+i,y+j,icon[j][i])
+                self.device.pixel(x+i,y+j,1 if icon[j][i]==color else 0)
 
     def set(self, topic:str, payload:str):
         logging.debug(f"{self}.set(topic='{topic}', payload = '{payload}')")
@@ -63,17 +63,23 @@ class Display:
             self.device.poweron()
         else:
             self.device.poweroff()
+    
 
 class Widget:
     def __init__(self, invert):
         self.root=None
         self.color = 0 if invert else 1
         self.backcolor = 1 if invert else 0
+        self.payload = None
     
     def set(self, payload:str):
         self.payload = payload
         self.show()
         self.root.update()
+    
+    def show(self, payload:str=None):
+        if payload:
+                self.payload = payload
 
 class Field(Widget):
     '''Un champ avec label
@@ -82,38 +88,43 @@ class Field(Widget):
                  row:int=0,column:int=0, width:int|None=None, height:int = 1,
                  align:int=LEFT,
                  invert = False,
+                 unit:str=None
                  ):
         super().__init__(invert)
-        self.label = label
+        self.label = label or ""
         self.row = row
         self.column = column
         self.height = height
         self.width = width
-        self.payload = None
         self.align = align
+        self.unit = unit or ""
 
     def show(self, payload:str=None):
-        if payload:
-            self.payload = payload
-        self.root.text(self.label or "", self.row, self.column, self.color, self.backcolor)
+        super().show(payload)
+        self.root.text(self.label, self.row, self.column, self.color, self.backcolor)
         text = self.payload or ""
         self.width = self.width or self.root.width_car - len(self.label) #Initialize once
-        text = text[:self.width]
+        text = text[:self.width] + self.unit
         if self.align == LEFT:
             text = text + " " * (self.width - len(text))
         else:
             text = " " * (self.width - len(text)) + text
         self.root.text(text, self.row, self.column + len(self.label), self.color, self.backcolor)
     
-
-class Icon(Widget):
-    '''Une icone
+class WidgetPix(Widget):
+    '''Un widget géré en x,y (alosr que Fields ets géré en row, colums)
     '''
-    def __init__(self, x:int,y:int, invert=False, icons:dict=None, function:function=None):
+    def __init__(self, x, y, invert):
         super().__init__(invert)
         self.x = x
         self.y = y
-        self.payload = None
+
+
+class Icon(WidgetPix):
+    '''Une icone
+    '''
+    def __init__(self, x:int,y:int, invert=False, icons:dict=None, function:function=None):
+        super().__init__(x, y, invert)
         self.icons = icons or {}
         self.f_icon = function
 
@@ -126,15 +137,60 @@ class Icon(Widget):
         self.f_icon = function
 
     def show(self, payload:str=None):
-        if payload:
-            self.payload = payload
+        super().show(payload)
         if self.payload in self.icons:
             self.root.draw_icon(self.icons[self.payload], self.x, self.y)
         elif self.f_icon:
             self.root.draw_icon(self.f_icon(self.payload), self.x, self.y)
+
+class BarGraph(WidgetPix):
+    '''Un barre graphe
+    '''
+    def __init__(self, x:int, y:int, lenght:int=100, width:int=10, n:int=10, max_value:float=1, orient:int=0, invert=False ):
+        '''
+        x,y     :   origine of the widget (top left corner)
+        lenght  :   lenght 
+        width   :   width
+        n       :   qty of rectangles
+        max_value : value for 100% full
+        orient  :   angle in degres (0:➡️ , 90:⬆️ , 180:⬅️, 270:⬇️)
+        '''
+        super().__init__(x, y, invert)
+        self.max_value = max_value
+        self.n = n
+        orient = orient % 360
+        if orient==0:
+            self.direction = lenght//n, 0
+            self.rect = lenght//n -1, width
+        elif orient==90:
+            self.y = self.y + lenght-lenght//n
+            self.direction = 0,-lenght//n
+            self.rect = width, lenght//n - 1
+        elif orient==180:
+            self.x = self.x + lenght-lenght//n
+            self.direction = -lenght//n, 0
+            self.rect = lenght//n-1, width
+        elif orient == 270:
+            self.direction = 0, lenght//n
+            self.rect = width, lenght//n -1
+        else:
+            raise Exception(f"orient must be in (0,90,180,270), not {orient}!")
+            
+    def show(self, payload:float=None):
+        '''payload: float between 0.0 and 1.0
+        '''
+        super().show(payload)
+        self.payload = float(self.payload or 0)
+        x, y = self.x, self.y
+        level:int = round(self.payload*self.n/self.max_value)
+        for index in range(self.n):
+            if index < level:
+                self.root.device.fill_rect(x,y, *self.rect, self.color)
+            else:
+                self.root.device.fill_rect(x+1,y+1, self.rect[0]-1, self.rect[1]-2, self.backcolor)
+                self.root.device.rect(x,y, *self.rect, self.color)
+            x, y = x + self.direction[0], y + self.direction[1]
         
-
-
 
 if __name__=='__main__':
     from lcd12864 import SPI_LCD12864
