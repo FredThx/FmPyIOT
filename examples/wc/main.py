9ae230c63090b65e3facf7dd37a49ed12f03d7dc
fredthx <fredthx@gmail.com>
2025-03-29 18:54:17 +0100

    fix : add params to wc

--- examples/wc/main.py
+++ examples/wc/main.py
@@ -5,15 +5,69 @@ from fmpyiot.topics import TopicAction
 import logging
 from devices.servo import Servo
 from devices.baregraph import BareGraph
-
+import json
 from credentials import CREDENTIALS
 
 time.sleep(5)
 
 assert len([])==0, "Error with len!"
 
-servo = Servo(28)
-bargraph = BareGraph([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+class AfficheurWC:
+    def __init__(self, servo: Servo, bargraph: BareGraph):
+        self.servo = servo
+        self.bargraph = bargraph
+        self.load_params()
+    
+    async def set_etat(self, payload):
+        if payload == 'libre':
+            await self.servo.move_async(self.params.get("angle_libre",80),speed = self.params.get("servo_speed"))
+            logging.info("WC libre")
+        else:
+            await self.servo.move_async(self.params.get("angle_occupe",170),speed = self.params.get("servo_speed"))
+            logging.info("WC occupé")
+
+    params = {
+        "angle_libre": 80,
+        "angle_occupe": 170,
+        "servo_speed": 0.5,
+    }
+    params_json = "params.json"
+
+    #TODO : incoprporer ce code à la classe FmPyIotWeb
+    def _load_params(self, params:dict):
+        '''Load les paramètres à partir d'un dict
+        en vérifiant le typage
+        '''
+        for k,v in params.items():
+            if k in self.params:
+                try:
+                    self.params[k] = type(self.params[k])(v) # type cast idem self.params
+                except ValueError:
+                    logging.error(f"Error with {k} : {v} cannot be converted to {type(self.params[k])}")
+            else:
+                logging.warning(f"Param {k} not in params")
+
+
+    def load_params(self, params:dict=None):
+        '''Initialise les paramètres
+            - a partir d'un fichier json
+            - ou d'un dict en entrée 
+        '''
+        if params:
+            self._load_params(params)
+        else:
+            try:
+                with open(self.params_json,"r") as json_file:
+                    self._load_params(json.load(json_file))
+            except OSError as e:
+                logging.error(str(e))
+        print(f"Params loaded. New params : {self.params}")
+
+afficheur_wc = AfficheurWC(
+    servo=Servo(28),
+    bargraph=BareGraph([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+)
 
 iot = FmPyIotWeb(
     mqtt_host = CREDENTIALS.mqtt_host,
@@ -29,13 +83,10 @@ iot = FmPyIotWeb(
     logging_level=logging.DEBUG,
     )
 
-def set_etat(payload):
-    if payload == 'libre':
-        servo.move(170-90) 
-    else:
-        servo.move(170)
-#TODO : parametres à ajuster pour le servo : speed et angles
-iot.add_topic(TopicAction("./ETAT", lambda topic, payload: set_etat(payload)))
-iot.add_topic(TopicAction("./BARGRAPH", lambda topic, payload: bargraph.set_level(int(payload))))
+
+iot.add_topic(TopicAction("./ETAT", lambda topic, payload: afficheur_wc.set_etat(payload)))
+iot.add_topic(TopicAction("./BARGRAPH", lambda topic, payload: afficheur_wc.bargraph.set_level(int(payload))))
+iot.set_param("afficheur", default=afficheur_wc.params, on_change=afficheur_wc.load_params)
+
 
 iot.run()
