83f53be3a9bc46282ed20411de9f2641d002b65d
fredthx <fredthx@gmail.com>
2025-05-25 11:41:53 +0200

    feat : reserve d'eau

---
+++ examples/serre_reserve_eau/main.py
@@ -0,0 +1,35 @@
+#from devices.ds18b20 import DS18b20
+#from devices.fdht import DHT22
+from machine import Pin, I2C
+from fmpyiot.fmpyiot_web import FmPyIotWeb
+from reservoir import Reservoir
+from devices.lps35hw import LPS35HW
+from devices.bmp085 import BMP180
+from credentials import CREDENTIALS
+import logging
+
+try:
+    len([])
+except:
+    del len
+
+i2c = I2C(1, scl=Pin(27), sda=Pin(26))
+lps = LPS35HW(i2c, address=0x5D)  # Capteur de pression au fond du réservoir
+bmp = BMP180(i2c, address=0x77)  # Capteur de pression hors du réservoir
+reservoir = Reservoir(lps, bmp, max_height=100, name="reservoir")
+
+iot = FmPyIotWeb(
+    mqtt_host = CREDENTIALS.mqtt_host,
+    ssid = CREDENTIALS.wifi_SSID,
+    password = CREDENTIALS.wifi_password,
+    web_credentials=(CREDENTIALS.web_user, CREDENTIALS.web_password),
+    mqtt_base_topic = "T-HOME/SERRE/RESERVOIR",
+    watchdog=100,
+    sysinfo_period = 600,
+    logging_level= logging.INFO,
+    name="Reservoir de la Serre de Fred",
+    description="Des capteur de pression pour mesurer le niveau d'eau dans le réservoir de la serre",
+    )
+
+reservoir.set_iot(iot)
+iot.run()
