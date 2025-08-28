8cfdd533ec8b8c3776b91882bef300d409a28f04
FredericOlfa <frederic.thome@olfa.fr>
2025-08-28 14:20:10 +0200

    fix : examples Olfa

--- examples/cuve_eau/main.py
+++ examples/cuve_eau/main.py
@@ -1,22 +1,26 @@
 from machine import I2C, Pin
 from devices.lidar import TF_Luna
-from fmpyiot.fmpyiot import FmPyIot
+from fmpyiot.fmpyiot_web import FmPyIotWeb
 from fmpyiot.topics import Topic
-
+import logging
 from credentials import CREDENTIALS
 
 i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq = 400_000)
 lidar = TF_Luna(i2c, min=20, max=800, freq = 100)
 
-iot = FmPyIot(            
+iot = FmPyIotWeb(
     mqtt_host = CREDENTIALS.mqtt_host,
+    mqtt_base_topic = "OLFA/ETANG_HAUT/NIVEAU",
     ssid = CREDENTIALS.wifi_SSID,
     password = CREDENTIALS.wifi_password,
-    web_credentials=(CREDENTIALS.web_user, CREDENTIALS.web_password),
-    mqtt_base_topic = "OLFA/ETANG_HAUT/NIVEAU",
-    watchdog=100,
+    watchdog=300,
     sysinfo_period = 600,
-    async_mode=False)
+    led_wifi='LED',
+    web_credentials=(CREDENTIALS.web_user, CREDENTIALS.web_password),
+    name = "La hauteur de l'étang du HAUT",
+    logging_level=logging.INFO,
+    )
+
 
 def get_distance():
     try:
@@ -25,9 +29,5 @@ def get_distance():
         return "Erreur capteur lidar"
 
 distance = Topic("./distance", read=lambda topic, payload : get_distance(), send_period=10)
-
-if True:
-    while not iot.connect():
-        print("Erreur lors de la connexion.... on retente!")
-    iot.add_topic(distance)
-    iot.run()
+iot.add_topic(distance)
+iot.run()
---
+++ examples/cuve_eau/old/main.py
@@ -0,0 +1,36 @@
+from machine import I2C, Pin
+import utime
+import sys
+from devices.lidar import TF_Luna
+
+from fmpyiot.fmpyiot import FmPyIot
+from fmpyiot.topics import Topic
+
+i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq = 400_000)
+lidar = TF_Luna(i2c, min=20, max=800, freq = 100)
+
+iot = FmPyIot(            
+    mqtt_host = "192.168.0.11",
+    mqtt_base_topic = "OLFA/ETANG_HAUT/NIVEAU",
+    ssid = 'OLFA_PRESSES',
+    password = "79073028",
+    #ssid = 'OLFA_WIFI',
+    #password = "Olfa08SignyLePetit",
+    watchdog=100,
+    sysinfo_period = 600,
+    async_mode=False)
+
+
+def get_distance():
+    try:
+        return lidar.distance()
+    except OSError:
+        return "Erreur"
+
+distance = Topic("./distance", read=lambda topic, payload : get_distance(), send_period=10)
+
+if True:
+    while not iot.connect():
+        print("Erreur lors de la connexion.... on retente!")
+    iot.add_topic(distance)
+    iot.run()
